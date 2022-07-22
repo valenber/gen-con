8dd4ce8c106820ff0c32358badd1662e2a7bc85a
Valentin Berlin <valentin.berlin@protonmail.com>
2022-07-22 08:48:49 +0200

    move applicant getter into own method

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Applicant struct {
-  ID string `json:"id"`
+  ID   string `json:"id"`
+  Template string `json:"templates"`
   Name string `json:"name"`
   AcceptanceDate string `json:"acceptance_date"`
 }
 
 var applicants = []Applicant{
-  {ID: "1", Name: "Janise Joplin", AcceptanceDate: "01-07-2022"},
-  {ID: "2", Name: "Robin Bobbin", AcceptanceDate: "21-03-2022"},
-  {ID: "3", Name: "Laura Maison", AcceptanceDate: ""},
-  {ID: "4", Name: "Harriet Tubman", AcceptanceDate: "12-10-2022"},
+  {ID: "1", Name: "Janise Joplin", Template: "1", AcceptanceDate: "01-07-2022"},
+  {ID: "2", Name: "Robin Bobbin", Template: "1", AcceptanceDate: "21-03-2022"},
+  {ID: "3", Name: "Laura Maison", Template: "1", AcceptanceDate: ""},
+  {ID: "4", Name: "Harriet Tubman", Template: "1", AcceptanceDate: "12-10-2022"},
 }
 
 func main()  {
@@ -29,13 +32,20 @@ func main()  {
 
 func getContract(c *gin.Context) {
   id := c.Param("applicant_id")
+  applicant, err := getApplicant(id)
 
-  for _, a := range applicants {
-    if a.ID == id {
-      c.IndentedJSON(http.StatusOK, a)
-      return
+  if err != nil {
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": fmt.Sprintf("Can not find applicant with id %s", id)})
+  } else {
+    c.IndentedJSON(http.StatusOK, applicant)
+  }
+}
+
+func getApplicant(id string) (*Applicant, error) {
+  for _, applicant := range applicants {
+    if applicant.ID == id {
+      return &applicant, nil 
     }
   }
-
-  c.IndentedJSON(http.StatusNotFound, gin.H{"message": "applicant no found"})
+  return nil, errors.New("Can not find applicant")
 }
